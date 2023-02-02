5e1cb71a9b2674af57557d2b31e4ac1a6c4baec7
Finn Bon <finn.bueno@gmail.com>
2023-02-02 22:28:33 +0100

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import './App.css';
+import { words } from './Words';
 
 function App() {
     const [wordSize, setWordSize] = useState(+(localStorage.getItem('wordSize') || '5'));
-    const [word, setWord] = useState(localStorage.getItem('currentWord'));
+
+    useEffect(() => {
+        setWord(newWord());
+    }, [wordSize]);
 
     const decreaseWordSize = () => {
         if (wordSize <= 5) return;
@@ -19,8 +23,48 @@ function App() {
         localStorage.setItem('wordSize', `${newWordSize}`);
     }
 
-    const nextWord = () => {
+    const [guesses, setGuesses] = useState<string[]>([]);
 
+    const newWord = () => {
+        let potentialWord;
+        do {
+            const list = words[wordSize];
+            potentialWord = list[Math.floor(Math.random() * list.length)];
+            const usedWords = localStorage.getItem('usedWords' + wordSize) || '';
+            if (usedWords.includes(potentialWord + ',')) {
+                potentialWord = undefined;
+            }
+        } while (!potentialWord);
+        const usedWords = localStorage.getItem('usedWords' + wordSize) || '';
+        localStorage.setItem('usedWords' + wordSize, usedWords + potentialWord + ',');
+
+        return potentialWord;
+    }
+
+    const [word, setWord] = useState(newWord());
+
+    const guess = () => {
+        if (guesses.length >= 5) return;
+        const guessValue: string = (document.getElementById('guessField')as any).value;
+        if (!guessValue) return;
+        setGuesses([
+            ...(guesses || []),
+            guessValue
+        ]);
+    }
+
+    const calculateColor = (rowNr: number, colNr: number) => {
+        if (colNr == 0) return '#d46934';
+        if (guesses[rowNr][colNr] === word[colNr]) {
+            return '#d46934';
+        }
+        const left = [...word];
+        for (let i = 0; i < word.length; i++) {
+            if (left[i] == guesses[rowNr][i]) {
+                left[i] = '.';
+            }
+        }
+        return word.includes(guesses[rowNr][colNr]) ? '#d4a934' : '#2593c0';
     }
 
     return (
@@ -30,29 +74,34 @@ function App() {
                 style={{
                     gridColumn: 2,
                     gridRow: 1,
-                    textAlign: 'center'
+                    textAlign: 'center',
+                    display: 'grid',
+                    gridTemplateColumns: '1fr',
+                    gridTemplateRows: 'auto 1fr'
                 }}
             >
                 <div style={{
                     display: 'grid',
-                    gridTemplateColumns: '1fr 1fr 1fr'
+                    gridRow: 1,
+                    gridColumn: 1,
+                    gridTemplateColumns: '1fr 1fr 1fr',
+                    padding: '24px 0',
                 }}>
                     <Flex
                         justifyContent='flex-start'
                         alignItems='center'
-                        bg='yellow'
                     >
-                        <button onClick={decreaseWordSize}>
-                            -1
-                        </button>
+                        <Button onClick={decreaseWordSize}>
+                            -
+                        </Button>
                         <p style={{ margin: '0 12px' }}>
                             <b>
                                 {wordSize}
                             </b>
                         </p>
-                        <button onClick={increaseWordSize}>
-                            +1
-                        </button>
+                        <Button onClick={increaseWordSize}>
+                            +
+                        </Button>
                     </Flex>
                     <h1 style={{ fontSize: '3rem', margin: 0 }}>
                         Lingo
@@ -60,18 +109,92 @@ function App() {
                     <Flex
                         justifyContent='flex-end'
                         alignItems='center'
-                        bg='red'
                     >
-                        <button onClick={nextWord}>
-                            Next word
-                        </button>
+                        <Button onClick={() => {
+                            setGuesses([]);
+                            setWord(newWord());
+                        }}>
+                            Nieuw woord
+                        </Button>
                     </Flex>
+                </div>
+                <div style={{
+                    gridColumn: 1,
+                    gridRow: 2,
+                    display: 'grid',
+                    gridTemplateColumns: `repeat(${wordSize}, 1fr)`,
+                    gridTemplateRows: `repeat(${7}, 2fr) 1fr`
+                }}>
+                    {[...Array(7).keys()].map(rowNr => 
+                        [...Array(wordSize).keys()].map(colNr => rowNr !== 5 ? (
+                            <Cell
+                                key={rowNr + '-' + colNr}
+                                style={{
+                                    backgroundColor: colNr == 0 && rowNr <= guesses.length ? '#d46934' : rowNr >= guesses.length ? '#2593c0' : calculateColor(rowNr, colNr),
+                                    gridColumn: colNr + 1,
+                                    gridRow: rowNr + 1
+                                }}
+                            >
+                                <Letter>
+                                    {rowNr <= guesses.length && colNr == 0 && word[0].toUpperCase()}
+                                    {guesses.length > rowNr && colNr != 0 ? guesses[rowNr][colNr].toUpperCase() : ''}
+                                    {guesses.length >= 5 && rowNr == 6 && word[colNr].toUpperCase()}
+                                </Letter>
+                            </Cell>
+                        ) : (
+                            <div />
+                        ))
+                    )}
+                    <div style={{
+                        gridColumn: `1 / span ${wordSize}`,
+                        gridRow: 8,
+                        display: 'flex',
+                        alignItems: 'center',
+                        padding: '0 12px'
+                    }}>
+                        <input type='text' id='guessField' style={{
+                            flexGrow: 1,
+                            fontSize: '2rem',
+                            borderRadius: '8px',
+                            margin: '0 24px 0 0',
+                            padding: '0 4px'
+                        }} />
+                        <Button onClick={guess}>
+                            Raad!
+                        </Button>
+                    </div>
                 </div>
             </div>
             <div style={{ gridColumn: 3, gridRow: 1 }} />
         </OutsideGrid >
     );
 }
+
+const Letter = styled.p`
+font-weight: bold;
+font-size: 4rem;
+margin: 0;
+`;
+
+const Cell = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+margin: 12px;
+background-color: #2593c0;
+cursor: pointer;
+`;
+
+const Button = styled.button`
+background-color: #2593c0;
+border: none;
+font-size: 2rem;
+cursor: pointer;
+border-radius: 8px;
+&:active {
+    transform: translateY(5px);
+}
+`;
 
 const Flex = styled.div`
 display: flex;
