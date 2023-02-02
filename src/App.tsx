a376d4560c03b96456b0ab9f2f7bda63dfc2bd68
FinnBon <finn.bon@kabood.io>
2023-02-02 15:37:01 +0100

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,57 @@ import './App.css';
 
 function App() {
     return (
-        <Grid>
-            Test
-        </Grid>
+        <OutsideGrid>
+            <div style={{ gridColumn: 1, gridRow: 1 }} />
+            <div
+                style={{
+                    gridColumn: 2,
+                    gridRow: 1,
+                    textAlign: 'center'
+                }}
+            >
+                <div style={{
+                    display: 'grid',
+                    gridTemplateColumns: '1fr 1fr 1fr'
+                }}>
+                    <Flex
+                        justifyContent='flex-start'
+                        alignItems='center'
+                        bg='yellow'
+                    >
+                        <p>Increase word size</p>
+                    </Flex>
+                    <h1 style={{ fontSize: '3rem', margin: 0 }}>
+                        Lingo
+                    </h1>
+                    <Flex
+                        justifyContent='flex-end'
+                        alignItems='center'
+                        bg='red'
+                    >
+                        <p>Next word</p>
+                    </Flex>
+                </div>
+            </div>
+            <div style={{ gridColumn: 3, gridRow: 1 }} />
+        </OutsideGrid >
     );
 }
-const Grid = styled.div`
+
+const Flex = styled.div`
+display: flex;
+justify-content: ${(props: any) => props.justifyContent};
+align-items: ${(props: any) => props.alignItems};
+background-color: ${(props: any) => props.bg};
+`
+
+const OutsideGrid = styled.div`
+background-color: #45b3e0;
+height: 100%;
+
 display: grid;
+grid-template-columns: 1fr min(100%, 800px) 1fr;
+grid-template-rows: 1fr;
 `;
 
 export default App;
